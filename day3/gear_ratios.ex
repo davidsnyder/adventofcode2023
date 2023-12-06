eb46b5c00ddc1a828c212800e9d9b2ddb0f32e09
David Snyder <davidsnyder.jet@gmail.com>
2023-12-05 19:08:43 -0600

    day3/puzzle1 solution

---
+++ day3/gear_ratios.ex
@@ -0,0 +1,58 @@
+defmodule GearRatios do
+  @regex ~r/[0-9.]/
+  def search_adjacent(matrix, row_index, row) do
+    height = map_size(matrix)
+    width = String.length(matrix[0])
+    #IO.puts("#{width}, #{height}")
+    words = Regex.scan(~r/[0-9]+/, row)
+    indexes = Regex.scan(~r/[0-9]+/, row, return: :index)
+    Enum.zip(List.flatten(words), List.flatten(indexes))
+    |> Enum.map(&GearRatios.generate_coordinates(row_index, &1))
+    |> Enum.map(fn {num, coords} -> {num, Enum.filter(coords, fn {x, y} -> x >= 0 and x < height and y >= 0 and y < width; end)} end)
+    |> Enum.map(fn {num, coords} -> {num, Enum.map(coords, fn {x, y} -> String.at(matrix[x], y) end)} end)
+    |> Enum.map(fn {num, chars} -> {num, Enum.filter(chars, fn char -> char != "." and not Regex.match?(@regex, char) end)} end)
+    |> Enum.filter(fn {_num, chars} -> length(chars) > 0 end)
+    |> Enum.map(&elem(&1, 0))
+    |> Enum.reduce(0, fn ele, sum -> sum + ele; end)
+
+  end
+
+  def generate_coordinates(x, {num, {y, length}}) do
+    coords = for n_x <- (x-1)..(x+1),
+      n_y <- (y-1)..(y+length) do
+        {n_x,n_y}
+      end
+      {String.to_integer(num), coords}
+  end
+end
+
+input = IO.read(:stdio, :eof)
+matrix_map = input
+  |> String.split("\n")
+  |> Enum.with_index()
+  |> Enum.to_list()
+  |> Enum.map(fn tup -> {elem(tup,1), elem(tup,0)}; end)
+  |> Map.new
+
+matrix_map
+  |> Enum.map(fn {k, v} ->  GearRatios.search_adjacent(matrix_map, k, v); end)
+  |> Enum.reduce(0, fn ele, sum -> sum + ele; end)
+  |> IO.puts
+
+  # load into 2D matrix
+  # 467..114..
+  # ...*......
+  # ..35..633.
+  # ......#...
+  # 617*......
+  # .....+.58.
+  # ..592.....
+  # ......755.
+  # ...$.*....
+  # .664.598..
+
+  # for 592 i need to check [5,1], 5,2 5,3 5,4 5,5
+         #                    [6,1] [6,5]
+          #                 [7,1], 7,2 7,3 7,4 7,5
+
+          # i have {2, 3} and 6 (row), so 6,2 and length 3
