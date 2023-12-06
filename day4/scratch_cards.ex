5543d3c769e8e67e26c08c4e057a5122b3240fbe
David Snyder <davidsnyder.jet@gmail.com>
2023-12-06 17:52:35 -0600

    day4/puzzle2

--- day4/scratch_cards.ex
+++ day4/scratch_cards.ex
@@ -1,5 +1,5 @@
 defmodule ScratchCards do
-  def calculate_points(winning_numbers, my_numbers) do
+  def calculate_matches(winning_numbers, my_numbers) do
     winning = winning_numbers
     |> Enum.map(&String.to_integer/1)
     |> Enum.zip(Stream.repeatedly(fn -> true end)|> Enum.take(length(winning_numbers)))
@@ -8,8 +8,28 @@ defmodule ScratchCards do
     |> Enum.map(&Map.has_key?(winning, &1))
     |> Enum.filter(fn e -> e; end)
     |> length()
+  end
+
+  # part 1
+  def calculate_num_cards(winning_numbers, my_numbers) do
+    calculate_matches(winning_numbers, my_numbers)
     |> Kernel.-(1)
     |> (fn x -> if x >= 0 do :math.pow(2, x) else 0 end end).()
+  end
+
+  # part 2
+  def generate_copies(queue, full_list, num_cards) do
+    if :queue.is_empty(queue) do # base case
+      num_cards
+    else # recur
+      {{_, val}, new_queue} = :queue.out(queue)
+      if elem(val, 0) > 0 do # winning numbers > 0
+        copies = Enum.slice(full_list, (elem(val, 1)+1)..(elem(val, 0)+elem(val, 1))) # get copies
+        generate_copies(:queue.join(new_queue, :queue.from_list(copies)), full_list, num_cards + 1)
+      else # without winning numbers there's no copies, just add to the total count
+        generate_copies(new_queue, full_list, num_cards + 1)
+      end
+    end
   end
 end
 
@@ -22,7 +42,7 @@ IO.read(:stdio, :eof)
   |> Enum.map(&String.split(&1, "|"))
   |> Enum.map(&Enum.map(&1, fn ele -> String.trim(ele) end))
   |> Enum.map(&Enum.map(&1, fn ele -> String.split(ele, ~r/ +/) end))
-  |> Enum.map(fn ele -> ScratchCards.calculate_points(hd(ele), List.last(ele)) end)
-  |> Enum.sum
-  |> trunc()
-  |> IO.inspect(charlists: :as_lists)
+  |> Enum.map(fn ele -> ScratchCards.calculate_matches(hd(ele), List.last(ele)) end)
+  |> Enum.with_index
+  |> (&ScratchCards.generate_copies(:queue.from_list(&1), &1, 0)).()
+  |> IO.inspect()
