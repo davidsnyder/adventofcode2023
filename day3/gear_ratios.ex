86ef38cc3172b18c19c7bf20f800ff631c3dbc05
David Snyder <davidsnyder.jet@gmail.com>
2023-12-05 22:46:33 -0600

    day3/puzzle2

--- day3/gear_ratios.ex
+++ day3/gear_ratios.ex
@@ -1,7 +1,5 @@
 defmodule GearRatios do
 
-  @non_symbol_regexp ~r/[0-9.]/
-
   def search_adjacent(matrix, row_index, row) do
     height = map_size(matrix)
     width = String.length(matrix[0])
@@ -10,11 +8,11 @@ defmodule GearRatios do
     Enum.zip(List.flatten(words), List.flatten(indexes))
     |> Enum.map(&GearRatios.generate_coordinates(row_index, &1))
     |> Enum.map(fn {num, coords} -> {num, Enum.filter(coords, fn {x, y} -> x >= 0 and x < height and y >= 0 and y < width; end)} end)
-    |> Enum.map(fn {num, coords} -> {num, Enum.map(coords, fn {x, y} -> String.at(matrix[x], y) end)} end)
-    |> Enum.map(fn {num, chars} -> {num, Enum.filter(chars, fn char -> char != "." and not Regex.match?(@non_symbol_regexp, char) end)} end)
+    |> Enum.map(fn {num, coords} -> {num, Enum.map(coords, fn {x, y} -> {{x,y}, String.at(matrix[x], y)} end)} end)
+    |> Enum.map(fn {num, chars} -> {num, Enum.filter(chars, fn {_coords, char} -> char == "*" end)} end)
     |> Enum.filter(fn {_num, chars} -> length(chars) > 0 end)
-    |> Enum.map(&elem(&1, 0))
-    |> Enum.reduce(0, fn ele, sum -> sum + ele; end)
+    |> Enum.map(fn {num, chars} -> {num, List.first(chars)} end)
+    |> Enum.map(fn {num, {{x,y}, _c}} -> {"#{x},#{y}", num} end)
   end
 
   # this also includes the coordinates of the number itself, but we'll just filter those out later
@@ -35,11 +33,6 @@ matrix_map = input
   |> Enum.map(fn tup -> {elem(tup,1), elem(tup,0)}; end)
   |> Map.new
 
-matrix_map
-  |> Enum.map(fn {k, v} ->  GearRatios.search_adjacent(matrix_map, k, v); end)
-  |> Enum.reduce(0, fn ele, sum -> sum + ele; end)
-  |> IO.puts
-
   # part 1
   # build a 2d matrix out of the input
   # find the indices of the numbers in it (using a regexp)
@@ -47,8 +40,29 @@ matrix_map
   # find the numbers that have symbols adjacent
   # sum these together
 
+  # matrix_map
+  # |> Enum.map(fn {k, v} ->  GearRatios.search_adjacent(matrix_map, k, v); end)
+  # |> Enum.reduce(0, fn ele, sum -> sum + ele; end)
+  # |> IO.puts
+
   # part 2
   # need to save the coordinates of the symbols (we already have them)
   # then see if two numbers share the same coordinates
   # if they do, multiply them together
   # then sum these together
+
+  matrix_map
+  |> Enum.map(fn {k, v} ->  GearRatios.search_adjacent(matrix_map, k, v); end)
+  |> List.flatten()
+  |> Enum.reduce(%{}, fn {key, value}, acc ->
+    case Map.get(acc, key) do
+      nil -> Map.put(acc, key, [value])
+      existing_list -> Map.put(acc, key, [value | existing_list])
+    end
+  end)
+  |> Map.values
+  |> Enum.filter(fn l -> length(l) == 2; end)
+  #|> IO.inspect(charlists: :as_lists)
+  |> Enum.map(fn [a, b] -> a * b; end)
+  |> Enum.sum()
+  |> IO.puts
