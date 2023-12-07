5fea652814de1ec68da6f20d4d539dac060b5278
David Snyder <davidsnyder.jet@gmail.com>
2023-12-07 13:32:50 -0600

    day6/puzzle2

--- day6/boat_charger.ex
+++ day6/boat_charger.ex
@@ -1,6 +1,6 @@
 defmodule BoatCharger do
   def charges(race) do
-    {time, record} = race
+    [time, record | _rest] = race
     {record, Enum.map(Range.to_list(0..time), fn t -> t * (time - t) end)}
   end
 end
@@ -12,12 +12,13 @@ IO.read(:stdio, :eof)
   |> List.flatten
   |> Enum.map(&String.trim(&1))
   |> Enum.map(&String.split(&1, ~r/ +/))
-  |> Enum.map(fn l -> Enum.map(l, fn e -> String.to_integer(e) end) end)
-  |> Enum.zip # [{7, 9}, {15, 40}, {30, 200}]
-  |> Enum.map(&BoatCharger.charges(&1))
-  |> Enum.map(fn e -> Enum.filter(elem(e, 1), fn t -> t > elem(e, 0) end) end)
-  |> Enum.map(&length/1)
-  |> Enum.reduce(1, fn e, acc -> e * acc end)
+  |> Enum.map(&Enum.join/1)
+  |> Enum.map(&String.to_integer/1)
+  #|> Enum.zip # [{7, 9}, {15, 40}, {30, 200}]
+  |> (&BoatCharger.charges(&1)).()
+  |> (&Enum.filter(elem(&1, 1), fn t -> t > elem(&1, 0) end)).()
+  |> (&length/1).()
+  #|> Enum.reduce(1, fn e, acc -> e * acc end)
   |> IO.inspect(charlists: :as_lists)
 
   # ex. i have 7 seconds, record is 9
