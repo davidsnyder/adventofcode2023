a31eb82a044aa15a61758c6c343570fe657ec5fd
David Snyder <davidsnyder.jet@gmail.com>
2023-12-07 16:23:19 -0600

    day7/puzzle2 progress

--- day7/camel_cards.ex
+++ day7/camel_cards.ex
@@ -1,20 +1,35 @@
 defmodule CamelCards do
+  @handsize 5
   @ranks [[5], [4,1], [3,2], [3,1,1], [2,2,1], [2,1,1,1], [1,1,1,1,1]]
-  @cards %{"2" => 2, "3" => 3, "4" => 4, "5" => 5, "6" => 6, "7" => 7, "8" => 8,
-          "9" => 9, "T" => 10, "J" => 11, "Q" => 12, "K" => 13, "A" => 14}
+ # @cards %{"2" => 2, "3" => 3, "4" => 4, "5" => 5, "6" => 6, "7" => 7, "8" => 8,
+         # "9" => 9, "T" => 10, "J" => 11, "Q" => 12, "K" => 13, "A" => 14}
+  # part 2
+  @wildcards %{"2" => 2, "3" => 3, "4" => 4, "5" => 5, "6" => 6, "7" => 7, "8" => 8,
+          "9" => 9, "T" => 10, "J" => 1, "Q" => 12, "K" => 13, "A" => 14}
 
   def parse_rank(hand_bet) do
     [hand, bet | _rest] = hand_bet
     bet_int = String.to_integer(bet)
-    rank = hand
+    hand_dist = hand
     |> String.split("")
     |> Enum.filter(fn s -> String.length(s) > 0 end)
     |> Enum.reduce(%{}, fn card, acc ->
       Map.update(acc, card, 1, &(&1 + 1))
     end)
-    |> Map.values()
-    |> Enum.sort(&>=/2)
-    |> (&Enum.find_index(Enum.reverse(@ranks), fn e -> e == &1 end)).()
+
+    # part 2: delete Js and add them to the left most value (unless there are 5 Js)
+    js = Map.pop(hand_dist, "J", 0)
+
+    rank = if elem(js, 0) < @handsize do
+        max_ele = Map.delete(hand_dist, "J") |> Enum.max_by(fn x -> elem(x, 1) * Map.get(@wildcards, elem(x, 0)) end)
+        wildcard_ele = %{elem(max_ele, 0) => elem(js, 0) + elem(max_ele, 1)}
+        Map.merge(Map.delete(hand_dist, "J"), wildcard_ele)
+        |> Map.values()
+        |> Enum.sort(&>=/2)
+        |> (&Enum.find_index(Enum.reverse(@ranks), fn e -> e == &1 end)).()
+      else
+        6 # 5 jokers are the strongest hand
+      end
 
     {hand, bet_int, rank}
   end
@@ -23,7 +38,7 @@ defmodule CamelCards do
     hand
     |> String.split("")
     |> Enum.filter(fn s -> String.length(s) > 0 end)
-    |> Enum.map(fn c -> Map.get(@cards, c) end)
+    |> Enum.map(fn c -> Map.get(@wildcards, c) end)
     #|> IO.inspect(charlists: :as_lists)
   end
 
@@ -47,15 +62,7 @@ IO.read(:stdio, :eof)
   |> Enum.map(&CamelCards.parse_rank(&1))
   |> Enum.sort(fn {a_hand, _, a_rank}, {b_hand, _, b_rank} -> if b_rank == a_rank, do: CamelCards.tiebreaker(b_hand,a_hand), else: b_rank > a_rank end)
   |> Enum.with_index(1)
+  |> IO.inspect(charlists: :as_lists, limit: :infinity)
   |> Enum.map(fn {{_h,b,_r},rr} -> b * rr end)
   |> Enum.sum
   |> IO.inspect
-
-
-# Five of a kind, where all five cards have the same label: AAAAA [5,0...]
-# Four of a kind, where four cards have the same label and one card has a different label: AA8AA [4,1,...]
-# Full house, where three cards have the same label, and the remaining two cards share a different label: 23332 [3,2..]
-# Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98 [3,1,1]
-# Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432 [2,2,1]
-# One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4 [2,1,1,1]
-# High card, where all cards' labels are distinct: 23456 [1,1,1,1]
