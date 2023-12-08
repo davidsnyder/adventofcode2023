e871ae8f926dc2f9c5fd5fda6df030c910a12676
David Snyder <davidsnyder.jet@gmail.com>
2023-12-08 11:06:44 -0600

    day8/puzzle1

--- day8/map_navigator.ex
+++ day8/map_navigator.ex
@@ -1,23 +1,42 @@
 defmodule MapNavigator do
+  @end_node "ZZZ"
+  @left_dir "L"
+
   def parse_network(network) do
-    [directions, nodes | _rest] = network
-    directions
+    [dirs_raw, nodes | _rest] = network
+
+    dirs = dirs_raw
     |> String.split("")
     |> Enum.filter(fn s -> String.length(s) > 0 end)
-    |> IO.inspect
-    nodes
+    |> Enum.map(fn x -> if x == @left_dir, do: 0, else: 1 end)
+
+    {dirs, nodes
     |> String.split("\n")
+    |> Enum.map(&String.split(&1, " = "))
+    |> Enum.map(&{Enum.at(&1, 0), parse_left_right(Enum.at(&1, 1))})
+    |> Map.new}
+  end
+
+  def step(node, step_index, graph, directions) do
+    if node == @end_node do
+      step_index
+    else
+      next_dir = Enum.at(directions, (if step_index > 0, do: rem(step_index, length(directions)), else: 0) )
+      next_step = Map.get(graph, node)
+      step(elem(next_step, next_dir), step_index + 1, graph, directions)
+    end
+  end
+
+  def parse_left_right(lr) do
+    lr
+    |> String.slice(1..-2)
+    |> String.split(", ")
+    |> (&{Enum.at(&1, 0), Enum.at(&1, 1)}).()
   end
 end
 
 IO.read(:stdio, :eof)
   |> String.split("\n\n")
   |> (&MapNavigator.parse_network(&1)).()
-  # |> Enum.map(&String.split(&1, " "))
-  # |> Enum.map(&CamelCards.parse_rank(&1))
-  # |> Enum.sort(fn {a_hand, _, a_rank}, {b_hand, _, b_rank} -> if b_rank == a_rank, do: CamelCards.tiebreaker(b_hand,a_hand), else: b_rank > a_rank end)
-  # |> Enum.with_index(1)
-  # #|> IO.inspect(charlists: :as_lists, limit: :infinity)
-  # |> Enum.map(fn {{_h,b,_r},rr} -> b * rr end)
-  # |> Enum.sum
+  |> (&MapNavigator.step("AAA", 0, elem(&1, 1), elem(&1, 0))).()
   |> IO.inspect
