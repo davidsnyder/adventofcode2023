f1218b7179af633d0da16d8e39db93cc93ca55b9
David Snyder <davidsnyder.jet@gmail.com>
2023-12-08 12:26:01 -0600

    calculate lcm

--- day8/map_navigator.ex
+++ day8/map_navigator.ex
@@ -20,7 +20,7 @@ defmodule MapNavigator do
     |> Map.new}
   end
 
-  def step_all_A(graph, directions) do
+  def step_all(graph, directions) do
     Map.keys(graph)
     |> Enum.filter(&Regex.match?(@begin_node, &1))
     |> (&step(Enum.zip(&1, Stream.repeatedly(fn -> 0 end)|> Enum.take(length(&1))), graph, directions)).()
@@ -32,9 +32,13 @@ defmodule MapNavigator do
     else
       advanced = Enum.map(nodes_and_steps, fn nas ->
         {node, step_index} = nas
+        if !Regex.match?(@end_node, node) do
         next_dir = Enum.at(directions, (if step_index > 0, do: rem(step_index, length(directions)), else: 0) )
         next_step = Map.get(graph, node)
         {elem(next_step, next_dir), step_index + 1}
+        else
+          nas
+        end
       end)
       step(advanced, graph, directions)
     end
@@ -46,12 +50,27 @@ defmodule MapNavigator do
     |> String.split(", ")
     |> (&{Enum.at(&1, 0), Enum.at(&1, 1)}).()
   end
+
+  # Function to calculate the LCM of a list of numbers
+  def calculate_lcm(numbers) do
+    # Use the MathHelpers.lcm function as the accumulator function in Enum.reduce
+    Enum.reduce(numbers, 1, &MathHelpers.lcm/2)
+  end
+end
+
+defmodule MathHelpers do
+	def gcd(a, 0), do: a
+	def gcd(0, b), do: b
+	def gcd(a, b), do: gcd(b, rem(a,b))
+
+	def lcm(0, 0), do: 0
+	def lcm(a, b), do: div((a*b), gcd(a,b))
 end
 
 IO.read(:stdio, :eof)
   |> String.split("\n\n")
   |> (&MapNavigator.parse_network(&1)).()
-  |> (&MapNavigator.step_all_A(elem(&1, 1), elem(&1, 0))).()
+  |> (&MapNavigator.step_all(elem(&1, 1), elem(&1, 0))).()
   |> Enum.map(&elem(&1, 1))
-  |> Enum.max
+  |> (&MapNavigator.calculate_lcm(&1)).()
   |> IO.inspect
