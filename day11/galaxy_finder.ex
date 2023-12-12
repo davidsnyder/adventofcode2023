52c378e2262dc219e60ed2c6145da28b804e75ff
David Snyder <davidsnyder.jet@gmail.com>
2023-12-12 11:55:32 -0600

    set expansion factor generically

--- day11/galaxy_finder.ex
+++ day11/galaxy_finder.ex
@@ -1,17 +1,19 @@
 defmodule GalaxyFinder do
   @galaxy "#"
   @alldots ~r/^\.+$/
+  @expansion_factor 2
+
   def expand(galaxy) do
     empty_columns = galaxy
     |> Enum.map(&Enum.map(Enum.slice(String.split(&1, ""), 1..-2), fn s -> if s == @galaxy do false else true end end))
     |> Enum.reduce(fn row, acc -> Enum.map(Enum.zip(row, acc), &(elem(&1,0) and elem(&1, 1))) end)
 
     galaxy
-    |> Enum.map(fn row -> if Regex.match?(@alldots, row), do: [row, row], else: row end)
+    |> Enum.map(fn row -> if Regex.match?(@alldots, row), do: List.duplicate(row, @expansion_factor), else: row end)
     |> List.flatten #double empty rows
     |> Enum.map(&Enum.slice(String.split(&1, ""), 1..-2))
     |> Enum.map(&Enum.with_index/1)
-    |> Enum.map(&Enum.map(&1, fn t -> if Enum.at(empty_columns, elem(t, 1)), do: [elem(t, 0), elem(t, 0)], else: elem(t, 0) end))
+    |> Enum.map(&Enum.map(&1, fn t -> if Enum.at(empty_columns, elem(t, 1)), do: List.duplicate(elem(t, 0), @expansion_factor), else: elem(t, 0) end)) #double empty columns
     |> Enum.map(&List.flatten/1)
   end
 
